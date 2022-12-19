55eff5eb3b54e3988ce4654dadee8db2af34e637
Tey Han Yee <hytey@techtants.com>
2022-12-19 14:55:40 +0800

    Initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { OidcModule } from 'nest-oidc-provider';
+import { OidcConfigModule } from './config/oidc-config.module';
+import { OidcConfigService } from './config/oidc-config.service';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InteractionModule } from './interaction/interaction.module';
 
 @Module({
-  imports: [],
+  imports: [
+    OidcModule.forRootAsync({
+      imports: [OidcConfigModule],
+      useExisting: OidcConfigService,
+    }),
+    InteractionModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { urlencoded } from 'express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '../', 'public'));
+  app.setBaseViewsDir(join(__dirname, '../', 'views'));
+  app.setViewEngine('ejs');
+
+  app.use('/interaction', urlencoded({ extended: false }));
+
+  const PORT = 3001;
+  await app.listen(PORT);
+
+  const logger = new Logger(`techtants-oidc-provider`);
+  logger.log(`Listening on port ${PORT}`);
+  logger.log(
+    `Discovery endpoint: http://localhost:${PORT}/oidc/.well-known/openid-configuration`,
+  );
 }
 bootstrap();
